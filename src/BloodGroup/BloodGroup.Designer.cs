2dbd87f31d5672fdbd8951cd3f05d0c0e8f3f549
ByPikod <realmonster81@gmail.com>
2022-01-12 01:55:18 +0300

    Yükleme
    Program bitirildi ve yüklendi.

---
+++ src/BloodGroup/BloodGroup.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace BloodGroup
+{
+    partial class BloodGroup
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BloodGroup));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kan Grubu";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "0";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "AB",
+            "0"});
+            this.comboBox1.Location = new System.Drawing.Point(14, 71);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 36);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.updateList);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(229, 28);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 32);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Rh+";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.updateList);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(229, 75);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 32);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Rh-";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Location = new System.Drawing.Point(14, 163);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 144);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kan Alabilir:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kan Verebilir:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 28;
+            this.listBox2.Location = new System.Drawing.Point(174, 163);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(138, 144);
+            this.listBox2.TabIndex = 8;
+            // 
+            // BloodGroup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 335);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BloodGroup";
+            this.Text = "Kan Alışverişi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ src/BloodGroup/BloodGroup.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BloodGroup
+{
+    public partial class BloodGroup : Form
+    {
+        public BloodGroup()
+        {
+            
+            InitializeComponent();
+            Blood.createBloodGroups();
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        private void updateList(object sender, EventArgs e)
+        {
+            
+            BloodType type;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    type = BloodType.A;
+                    break;
+                case 1:
+                    type = BloodType.B;
+                    break;
+                case 2:
+                    type = BloodType.AB;
+                    break;
+                case 3:
+                    type = BloodType.ZERO;
+                    break;
+                default:
+                    type = BloodType.A;
+                    break;
+            }
+            Rh rh = Rh.NEGATIVE;
+            if (radioButton1.Checked) rh = Rh.POSITIVE;
+
+            Blood blood = Blood.getBlood(type, rh);
+
+            listBox1.Items.Clear();
+            foreach (Blood canTake in blood.canTake())
+            {
+                listBox1.Items.Add(canTake.ToString());
+            }
+
+            listBox2.Items.Clear();
+            foreach (Blood canGive in blood.CanGive())
+            {
+                listBox2.Items.Add(canGive.ToString());
+            }
+
+
+        }
+
+    }
+
+    public enum BloodType
+    {
+        A,
+        B,
+        AB,
+        ZERO
+    }
+
+    public enum Rh
+    {
+        POSITIVE,
+        NEGATIVE
+    }
+
+    public class Blood
+    {
+
+        public static List<Blood> bloodGroups = new List<Blood>();
+
+        BloodType type;
+        Rh rh;
+
+        public Blood(BloodType type, Rh rh)
+        {
+
+            this.type = type;
+            this.rh = rh;
+
+        }
+
+        public List<Blood> canTake()
+        {
+
+            List<Blood> ret = new List<Blood>();
+            ret.Add(getBlood(type, rh));
+
+            if (type == BloodType.ZERO)
+            {
+                if (rh == Rh.POSITIVE)
+                    ret.Add(getBlood(type, Rh.NEGATIVE));
+            }else if (type == BloodType.AB)
+            {
+
+                ret.Add(getBlood(BloodType.A, rh));
+                ret.Add(getBlood(BloodType.B, rh));
+                ret.Add(getBlood(BloodType.ZERO, rh));
+                if (rh == Rh.POSITIVE)
+                {
+
+                    ret.Add(getBlood(BloodType.A, Rh.NEGATIVE));
+                    ret.Add(getBlood(BloodType.B, Rh.NEGATIVE));
+                    ret.Add(getBlood(BloodType.AB, Rh.NEGATIVE));
+                    ret.Add(getBlood(BloodType.ZERO, Rh.NEGATIVE));
+
+                }
+
+            }
+            else
+            {
+                ret.Add(getBlood(BloodType.ZERO, Rh.NEGATIVE));
+
+                if (rh == Rh.POSITIVE)
+                {
+                    ret.Add(getBlood(BloodType.ZERO, Rh.POSITIVE));
+                    ret.Add(getBlood(type, Rh.NEGATIVE));
+                }
+            }
+
+            return ret;
+
+        }
+
+        public List<Blood> CanGive()
+        {
+
+            List<Blood> ret = new List<Blood>();
+
+            ret.Add(getBlood(type, rh));
+
+            if (type == BloodType.AB) {
+                if (rh == Rh.NEGATIVE)
+                    ret.Add(getBlood(type, Rh.POSITIVE));
+            }
+            else if (type == BloodType.ZERO)
+            {
+
+                ret.Add(getBlood(BloodType.A, rh));
+                ret.Add(getBlood(BloodType.B, rh));
+                ret.Add(getBlood(BloodType.AB, rh));
+
+                if (rh == Rh.NEGATIVE)
+                {
+
+                    ret.Add(getBlood(BloodType.A, Rh.POSITIVE));
+                    ret.Add(getBlood(BloodType.B, Rh.POSITIVE));
+                    ret.Add(getBlood(BloodType.AB, Rh.POSITIVE));
+                    ret.Add(getBlood(BloodType.ZERO, Rh.POSITIVE));
+
+                }
+
+            }
+            else
+            {
+
+                ret.Add(getBlood(BloodType.AB, rh));
+                if (rh == Rh.NEGATIVE)
+                {
+
+                    ret.Add(getBlood(type, Rh.POSITIVE));
+                    ret.Add(getBlood(BloodType.AB, Rh.POSITIVE));
+
+                }
+
+            }
+
+            return ret;
+
+        }
+
+        public static Blood getBlood(BloodType type, Rh rh)
+        {
+            
+            foreach (Blood blood in bloodGroups)
+            {
+                if(blood.type == type && blood.rh == rh) return blood;
+            }
+            return null;
+
+        }
+
+        public override string ToString()
+        {
+
+            string ret = "";
+            switch (type)
+            {
+                case BloodType.A:
+                    ret += "A";
+                    break;
+                case BloodType.B:
+                    ret += "B";
+                    break;
+                case BloodType.AB:
+                    ret += "AB";
+                    break;
+                case BloodType.ZERO:
+                    ret += "0";
+                    break;
+            }
+            if (rh == Rh.POSITIVE) ret += "+";
+            else
+                ret += "-";
+            return ret;
+
+        }
+
+        public static void createBloodGroups()
+        {
+
+            bloodGroups.Add(new Blood(BloodType.A, Rh.POSITIVE));
+            bloodGroups.Add(new Blood(BloodType.A, Rh.NEGATIVE));
+            bloodGroups.Add(new Blood(BloodType.B, Rh.POSITIVE));
+            bloodGroups.Add(new Blood(BloodType.B, Rh.NEGATIVE));
+            bloodGroups.Add(new Blood(BloodType.AB, Rh.POSITIVE));
+            bloodGroups.Add(new Blood(BloodType.AB, Rh.NEGATIVE));
+            bloodGroups.Add(new Blood(BloodType.ZERO, Rh.POSITIVE));
+            bloodGroups.Add(new Blood(BloodType.ZERO, Rh.NEGATIVE));
+
+        }
+
+    }
+}
